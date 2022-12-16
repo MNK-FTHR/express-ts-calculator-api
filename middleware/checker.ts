31a923e327db3d03f325e3b82342a3a41224f73c
MNK-FTHR <victorgranier.pierre@gmail.com>
2022-12-16 14:42:53 +0100

    final check

--- middleware/checker.ts
+++ middleware/checker.ts
@@ -1,6 +1,9 @@
 import {  Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction) =>{
+    if (req.body.calcul.length === 0) {
+        res.status(400).send("Pas de valeurs d'entrée");
+    }
     let valid = req.body.calcul.every(function(element: number) {return typeof element === 'number';})
     if (!valid) {
         res.status(400).send("Mauvaise valeur d'entrée");
